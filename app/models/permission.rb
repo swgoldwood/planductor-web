3287b421f6a5be2f5902c9cdd59e13d8e5b37448
Sebastian Warren <seb@wimedo.com>
2013-12-20 23:41:45 +0000

    Now permissions accept block to do more powerful permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,30 +1,34 @@
-class Permission < Struct.new(:user)
+class Permission
 
-  def initialize(user)
+  def initialize(user = nil)
     if user and user.admin
       allow_all
     elsif user and user.organiser
-      allow :competitions, [:all]
+      allow :competitions, [:new, :create]
+      allow :competitions, [:edit, :update, :destroy] do |comp|
+        comp.user_id == user.id
+      end
       allow :domains, [:all]
     end
     allow [:competitions, :domains], [:index, :show]
     allow [:planners, :users, :sessions, :participants], [:all]
   end
 
-  def allow?(controller, action)
-    @allow_all || @allowed_actions[[controller.to_s, action.to_s]] ||
-      @allowed_actions[[controller.to_s, :all.to_s]]
+  def allow?(controller, action, resource = nil)
+    allowed = @allow_all || @allowed_actions[[controller.to_s, action.to_s]] ||
+                @allowed_actions[[controller.to_s, :all.to_s]]
+    allowed && (allowed == true || resource && allowed.call(resource))
   end
 
   def allow_all
     @allow_all = true
   end
 
-  def allow(controllers, actions)
+  def allow(controllers, actions, &block)
     @allowed_actions ||= {}
     Array(controllers).each do |controller|
       Array(actions).each do |action|
-        @allowed_actions[[controller.to_s, action.to_s]] = true
+        @allowed_actions[[controller.to_s, action.to_s]] = block || true
       end
     end
   end
