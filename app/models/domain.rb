4f54c8483f17ca0b1b57c2710e721708eb0a5b41
Sebastian Warren <seb@wimedo.com>
2014-02-12 15:34:33 +0000

    validation specific status methods

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,5 +1,5 @@
 class Domain < ActiveRecord::Base
-  attr_accessible :name, :user_id, :number_of_problems, :tarball, :status
+  attr_accessible :name, :user_id, :number_of_problems, :tarball, :status, :plain_text
 
   has_attached_file :tarball
 
@@ -12,8 +12,22 @@ class Domain < ActiveRecord::Base
 
   after_initialize :init
 
+  def status_string
+    case self.status
+      when 'pending'
+        'Pending verification'
+      when 'verified'
+        'Successfully verified! Domain can be used in competitions.'
+      when 'error'
+        'There is a problem with domain uploaded. Please check documentation here'
+      else
+        'Unknown status'
+    end
+  end
+
   private
     def init
       self.status ||= 'pending'
+      self.number_of_problems ||= 0
     end
 end
