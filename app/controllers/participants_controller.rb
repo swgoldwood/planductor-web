c43627dbc7a48ca0aee2728f2787a2403055982b
Sebastian Warren <seb@wimedo.com>
2014-02-12 11:24:55 +0000

    competition participants added only when planner is verified

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,19 +1,30 @@
 class ParticipantsController < ApplicationController
   def create
-    @participant = Participant.new(params[:participant])
-    @competition = Competition.find(params[:participant][:competition_id])
+    @planner = Planner.find_by_id(params[:participant][:planner_id])
 
-    respond_to do |format|
-      if @participant.save
-        flash[:success] = 'Planner was successfully added.'
-        format.html { redirect_to @competition }
-      else
-        flash[:warning] = 'There was a problem. Planner was not added'
-        format.html { redirect_to @competition }
+    if current_user.id != @planner.user_id
+      unauthorized
+    else
+      @participant = Participant.new(params[:participant])
+      @competition = Competition.find(params[:participant][:competition_id])
+
+      respond_to do |format|
+        if @participant.save
+          flash[:success] = 'Planner was successfully added.'
+          format.html { redirect_to @competition }
+        else
+          flash[:warning] = 'There was a problem. Planner was not added'
+          format.html { redirect_to @competition }
+        end
       end
     end
   end
 
   def destroy
   end
+
+  private
+    def current_resource
+      @current_resource ||= Participant.find_by_id(params[:id]) if params[:id]
+    end
 end
