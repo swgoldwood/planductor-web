35154e32aa5d6cfa29ba1390553baa86429cac83
Sebastian Warren <seb@wimedo.com>
2014-02-12 15:35:58 +0000

    added plain text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140211123434) do
+ActiveRecord::Schema.define(:version => 20140212112907) do
 
   create_table "competitions", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20140211123434) do
     t.integer  "tarball_file_size"
     t.datetime "tarball_updated_at"
     t.string   "status"
+    t.text     "plain_text"
   end
 
   create_table "experiments", :force => true do |t|
