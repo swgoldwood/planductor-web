f0b9f5f205d0b7e65996bacedf52f572f3bef9dc
Sebastian Warren <seb@wimedo.com>
2014-02-12 11:25:21 +0000

    location not needed from planner

--- db/migrate/20140130163946_remove_location_from_planner.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLocationFromPlanner < ActiveRecord::Migration
-  def up
-    remove_column :planners, :location
-  end
-
-  def down
-    add_column :planners, :location, :string
-  end
-end
