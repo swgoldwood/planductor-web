78be3ccb1e5d372a946cd05d9bba4a60fe5bb5ac
Seb <swgoldwood@poseidon.london-entertainers.com>
2014-03-07 13:18:10 +0000

    added score chart. calculates score 0-1 for all participants. This is the standard way of evaluating and comparing planner performance in a competition

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,6 +1,8 @@
 class Experiment < ActiveRecord::Base
   attr_accessible :competition_id, :domain_id, :problem_id
 
+  has_many :tasks
+
   validates :competition_id, presence: true
   validates :domain_id, presence: true
   validates :problem_id, presence: true
@@ -8,4 +10,17 @@ class Experiment < ActiveRecord::Base
   belongs_to :competition
   belongs_to :domain
   belongs_to :problem
+
+  def best_quality
+    quality = nil
+    self.tasks.each do |task|
+      task.results.each do |result|
+        if result.valid_plan and (quality == nil or result.score < quality)
+          quality = result.score
+        end
+      end
+    end
+
+    quality
+  end
 end
---
+++ app/models/planductor_chart.rb
@@ -0,0 +1,56 @@
+module PlanductorChart
+  def self.quality_chart(competition, participant=nil)
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('string', 'Experiment')
+
+    competition.participants.each do |participant|
+      data_table.new_column('number', participant.planner.name)
+    end
+
+    count = 1 #TODO really don't like how I'm numbering the tasks per competition...
+    competition.experiments.each do |experiment|
+      row = ['Experiment ' + count.to_s]
+
+      competition.participants.each do |participant|
+        row.push(participant.best_score(experiment.id))
+      end
+
+      data_table.add_row(row)
+      count += 1
+    end
+
+    option = { title: 'Participant quality values for each experiment' }
+
+    GoogleVisualr::Interactive::BarChart.new(data_table, option)
+  end
+
+  def self.score_chart(competition, participant=nil)
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('string', 'Experiment')
+
+    competition.participants.each do |participant|
+      data_table.new_column('number', participant.planner.name)
+    end
+
+    count = 1
+    competition.experiments.each do |experiment|
+      row = ['Experiment ' + count.to_s]
+      best_quality = experiment.best_quality
+
+      competition.participants.each do |participant|
+        if best_quality == nil
+          row.push(nil)
+        else
+          row.push(best_quality.to_f / participant.best_score(experiment.id))
+        end
+      end
+
+      data_table.add_row(row)
+      count += 1
+    end
+
+    option = { title: 'Participant scores for each experiment', hAxis: { viewWindow: { max: 1}  } }
+
+    GoogleVisualr::Interactive::BarChart.new(data_table, option)
+  end
+end
