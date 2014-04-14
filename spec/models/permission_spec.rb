1f61340fad3702c5333bc85209f7a9e92056d3ac
Sebastian Warren <seb@wimedo.com>
2014-04-14 17:12:42 +0100

    adding some tests

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -11,5 +11,34 @@ describe Permission do
     subject { Permission.new(nil) }
 
     it { should allow_permission(:competitions, :index) }
+    it { should allow_permission(:domains, :index) }
+    it { should allow_permission(:sessions, :new) }
+    it { should allow_permission(:users, :new) }
+
+    it { should_not allow_permission(:competitions, :new) }
+    it { should_not allow_permission(:competitions, :destroy) }
+    it { should_not allow_permission(:domains, :new) }
+    it { should_not allow_permission(:users, :destroy) }
+  end
+
+  describe 'as organiser' do
+    let(:user) { create(:organiser) }
+
+    subject { Permission.new(user) }
+
+    it { should allow_permission(:domains, :new) }
+    it { should allow_permission(:competitions, :new) }
+    it { should allow_permission(:experiments, :new) }
+  end
+
+  describe 'as admin' do
+    let(:user) { create(:admin) }
+
+    subject { Permission.new(user) }
+
+    it { should allow_permission(:competitions, :new) }
+    it { should allow_permission(:domains, :new) }
+    it { should allow_permission(:domains, :anything) }
+    it { should allow_permission(:absolutely, :anything) }
   end
 end
