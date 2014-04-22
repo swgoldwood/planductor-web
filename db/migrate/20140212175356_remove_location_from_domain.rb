5370fc24f64f1c6fca4fc5165723612de6eff18b
Sebastian Warren <seb@wimedo.com>
2014-04-22 13:46:51 +0100

    removing pointless migration (PG was complaining about it). using postgres as database

--- db/migrate/20140212175356_remove_location_from_domain.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLocationFromDomain < ActiveRecord::Migration
-  def up
-    remove_column :domains, :location
-  end
-
-  def down
-    add_column :domains, :location, :string
-  end
-end
